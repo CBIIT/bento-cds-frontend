6ee1d62da3a00aa95d1dd8643bbc2d1c6ae859e3
huuaho <32457341+huuaho@users.noreply.github.com>
2023-11-13 12:17:51 -0500

    dynamically renders top value based on govt banner

--- src/components/NavBar/NavBarView.js
+++ src/components/NavBar/NavBarView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavBar } from 'bento-components';
 import {
   navBarData, navBarCartData, navBarstyling, externalLinks,
@@ -6,26 +6,53 @@ import {
 import Login from '../GoogleAuth/loginComponent';
 import globalData from '../../bento/siteWideConfig';
 
-const BentoNavBar = ({ cartFieldIds }) => (
-  <>
-    {globalData.enableAuthentication ? (
-      <NavBar
-        navBarData={navBarData}
-        navBarCartData={navBarCartData}
-        navBarstyling={navBarstyling}
-        numberOfCases={cartFieldIds.length || 0}
-        LoginComponent={Login}
-      />
-    ) : (
-      <NavBar
-        navBarData={navBarData}
-        navBarCartData={navBarCartData}
-        navBarstyling={navBarstyling}
-        externalLinksFlag
-        externalLinks={externalLinks}
-        numberOfCases={cartFieldIds.length || 0}
-      />
-    ) }
-  </>
-);
+const BentoNavBar = ({ cartFieldIds }) => {
+  const govAlertEl = document.getElementById('govAlertMsg');
+  const headerHeight = 100;
+  const initialTopValue = headerHeight + (govAlertEl ? govAlertEl.scrollHeight : 0);
+  const [topValue, setTopValue] = useState(initialTopValue);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Calculate the new top value based on scroll position
+      const scrolledDownAmt = window.scrollY;
+      const newTopValue = Math.max(headerHeight, initialTopValue - scrolledDownAmt);
+      setTopValue(newTopValue);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [initialTopValue]);
+
+  const scrollingStyle = {
+    ...navBarstyling.global,
+    marginTop: `${topValue}px`,
+  };
+
+  return (
+    <>
+      {globalData.enableAuthentication ? (
+        <NavBar
+          navBarData={navBarData}
+          navBarCartData={navBarCartData}
+          navBarstyling={{ ...navBarstyling, global: scrollingStyle }}
+          numberOfCases={cartFieldIds.length || 0}
+          LoginComponent={Login}
+        />
+      ) : (
+        <NavBar
+          navBarData={navBarData}
+          navBarCartData={navBarCartData}
+          navBarstyling={{ ...navBarstyling, global: scrollingStyle }}
+          externalLinksFlag
+          externalLinks={externalLinks}
+          numberOfCases={cartFieldIds.length || 0}
+        />
+      ) }
+    </>
+  );
+};
 export default BentoNavBar;
--- src/components/Stats/StatsView.js
+++ src/components/Stats/StatsView.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatsBar } from 'bento-components';
 import { statsStyling, globalStatsData } from '../../bento/globalStatsData';
 
-const StatsView = ({ data }) => (
-  <>
-    <StatsBar
-      data={data}
-      globalStatsData={globalStatsData}
-      statsStyling={statsStyling}
-    />
-  </>
-);
+const StatsView = ({ data }) => {
+  const govAlertEl = document.getElementById('govAlertMsg');
+  const headerHeight = 100;
+  const navBarHeight = 39;
+  const initialTopValue = headerHeight + navBarHeight + (govAlertEl ? govAlertEl.scrollHeight : 0);
+  const [topValue, setTopValue] = useState(initialTopValue);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Calculate the new top value based on scroll position
+      const scrolledDownAmt = window.scrollY;
+      const newTopValue = Math.max(headerHeight + navBarHeight, initialTopValue - scrolledDownAmt);
+
+      setTopValue(newTopValue);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [initialTopValue]);
+
+  const scrollingStyle = {
+    ...statsStyling.global,
+    top: `${topValue}px`,
+  };
+
+  return (
+    <>
+      <StatsBar
+        data={data}
+        globalStatsData={globalStatsData}
+        statsStyling={{ ...statsStyling, global: scrollingStyle }}
+      />
+    </>
+  );
+};
 
 export default StatsView;
