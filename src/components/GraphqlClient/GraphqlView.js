1d5e4b949b8950c841efc6b9d7644fc6e2137804
Alejandro-Vega <vegaalejandro15@gmail.com>
2024-02-28 12:55:15 -0500

    Revert GraphiQL changes

--- src/components/GraphqlClient/GraphqlView.js
+++ src/components/GraphqlClient/GraphqlView.js
@@ -6,39 +6,6 @@ import env from '../../utils/env';
 
 const BACKEND = env.REACT_APP_BACKEND_API;
 
-const DEFAULT_QUERY = `# Welcome to GraphQL
-#
-# GraphQL is an in-browser tool for writing, validating, and
-# testing GraphQL queries.
-#
-# Type queries into this side of the screen, and you will see intelligent
-# typeaheads aware of the current GraphQL type schema and live syntax and
-# validation errors highlighted within the text.
-#
-# GraphQL queries typically start with a "{" character. Lines that start
-# with a # are ignored.
-#
-# An example GraphQL query might look like:
-#
-#     {
-#       field(arg: "value") {
-#         subField
-#       }
-#     }
-#
-# Keyboard shortcuts:
-#
-#   Prettify query:  Shift-Ctrl-P (or press the prettify button)
-#
-#  Merge fragments:  Shift-Ctrl-M (or press the merge button)
-#
-#        Run Query:  Ctrl-Enter (or press the play button)
-#
-#    Auto Complete:  Ctrl-Space (or just start typing)
-#
-
-`;
-
 function graphQLFetcher(graphQLParams) {
   return fetch(BACKEND, {
     method: 'post',
@@ -47,17 +14,7 @@ function graphQLFetcher(graphQLParams) {
   }).then((response) => response.json());
 }
 
-const GraphqlView = ({ classes }) => (
-  <div className={classes.grapqhQlContainer}>
-    <GraphiQL
-      editorTheme="solarized light"
-      fetcher={graphQLFetcher}
-      defaultQuery={DEFAULT_QUERY}
-    >
-      <GraphiQL.Logo>GraphQL</GraphiQL.Logo>
-    </GraphiQL>
-  </div>
-);
+const GraphqlView = ({ classes }) => (<div className={classes.grapqhQlContainer}><GraphiQL editorTheme="solarized light" fetcher={graphQLFetcher} /></div>);
 
 const styles = () => ({
   grapqhQlContainer: {
