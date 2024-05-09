ab88af1d645d4defb6812923dd43d0da497e8501
Alejandro-Vega <vegaalejandro15@gmail.com>
2024-05-09 15:51:23 -0400

    Updated logo to datahub logo

--- src/bento/globalHeaderData.js
+++ src/bento/globalHeaderData.js
@@ -3,7 +3,7 @@ import Easter from '../assets/header/headerGraphic.png';
 // globalHeaderLogo image 468x100
 // globalHeaderImage: image 2200x100
 export default {
-  globalHeaderLogo: 'https://raw.githubusercontent.com/CBIIT/datacommons-assets/main/cds/logo/cds-logo.svg',
+  globalHeaderLogo: 'https://raw.githubusercontent.com/CBIIT/datacommons-assets/main/cds/logo/dh-logo.svg',
   globalHeaderLogoLink: '/',
   globalHeaderLogoAltText: 'CDS Logo',
   globalHeaderImage: Easter,
