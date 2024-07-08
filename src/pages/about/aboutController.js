66e39ac5aa50a964833b27f26cd08bc747b9a754
Alejandro-Vega <vegaalejandro15@gmail.com>
2024-07-08 15:30:03 -0400

    Fix aboutController rendering view before data is ready

--- src/pages/about/aboutController.js
+++ src/pages/about/aboutController.js
@@ -3,29 +3,37 @@ import yaml from 'js-yaml';
 import axios from 'axios';
 import YAMLData from '../../content/prod/aboutPagesContent.yaml';
 import AboutView from './aboutView';
+import { CircularProgress } from '@material-ui/core';
 
 const About = ({ match }) => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       let resultData = [];
       let result = [];
       try {
+        setLoading(true);
         result = await axios.get(YAMLData);
         resultData = yaml.safeLoad(result.data);
         const supportObj = resultData.find(({ page }) => page === match.path);
         setData(supportObj);
       } catch (error) {
         return setData({error})
+      } finally {
+        setLoading(false);
       }
     };
     fetchData();
   }, [match.path]);
 
+  console.log({ data });
   if(data.error){
     return <div>Error in Loading aboutPagesContent.yaml.</div>
   }
+
+  if (loading) return <CircularProgress />;
 
   return (
 
