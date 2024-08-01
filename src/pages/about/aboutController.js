ec0288bbf9900b568b70cdb2bd6b54f65eeb91d5
Alejandro-Vega <vegaalejandro15@gmail.com>
2024-08-01 16:46:10 -0400

    Added ejs templating to the about pages yaml file to map the links to the static content based on env variables

--- src/pages/about/aboutController.js
+++ src/pages/about/aboutController.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import yaml from 'js-yaml';
 import axios from 'axios';
+import ejs from 'ejs';
 import YAMLData from '../../content/prod/aboutPagesContent.yaml';
 import AboutView from './aboutView';
 import { CircularProgress } from '@material-ui/core';
+import { STATIC_CONTENT } from '../../assets/staticContent';
+
+const STATIC_CONTENT_MAPPING = {
+  USER_GUIDE_LINK: STATIC_CONTENT.about.USER_GUIDE,
+}
 
 const About = ({ match }) => {
   const [data, setData] = useState([]);
@@ -16,7 +22,8 @@ const About = ({ match }) => {
       try {
         setLoading(true);
         result = await axios.get(YAMLData);
-        resultData = yaml.safeLoad(result.data);
+        const renderedContent = ejs.render(result.data, STATIC_CONTENT_MAPPING);
+        resultData = yaml.safeLoad(renderedContent);
         const supportObj = resultData.find(({ page }) => page === match.path);
         setData(supportObj);
       } catch (error) {
